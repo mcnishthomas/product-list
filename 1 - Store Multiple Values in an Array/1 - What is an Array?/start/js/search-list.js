ec0cf369ca271ea59df896ba450c0a494d289b07
Thomas McNish <mcnishthomas@gmail.com>
2020-11-30 08:04:32 -0500

    Fixed null error that was appearing

--- 1 - Store Multiple Values in an Array/1 - What is an Array?/start/js/search-list.js
+++ 1 - Store Multiple Values in an Array/1 - What is an Array?/start/js/search-list.js
@@ -19,8 +19,12 @@ const inStock = [
 const search = prompt("Search for a product.");
 let message;
 
-if (inStock.includes(search)) {
-  message = `Yes, we have <strong>${search}</strong>.`;
+if (!search) {
+  message = `<strong>In stock:</strong> ${inStock.join(", ")}`;
+} else if (inStock.includes(search.toLowerCase())) {
+  message = `Yes, we have <strong>${search}</strong>. It's # ${
+    inStock.indexOf(search.toLowerCase()) + 1
+  } on the list!`;
 } else {
   message = `Sorry, we do not have <strong>${search}</strong>.`;
 }
