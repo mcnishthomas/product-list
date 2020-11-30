8166b38efd28b111f7e6f4c943b67d760b6c03af
Thomas McNish <mcnishthomas@gmail.com>
2020-11-30 08:23:10 -0500

    added toLowerCase functionality

--- 1 - Store Multiple Values in an Array/1 - What is an Array?/start/js/search-list.js
+++ 1 - Store Multiple Values in an Array/1 - What is an Array?/start/js/search-list.js
@@ -19,12 +19,8 @@ const inStock = [
 const search = prompt("Search for a product.");
 let message;
 
-if (!search) {
-  message = `<strong>In stock:</strong> ${inStock.join(", ")}`;
-} else if (inStock.includes(search.toLowerCase())) {
-  message = `Yes, we have <strong>${search}</strong>. It's # ${
-    inStock.indexOf(search.toLowerCase()) + 1
-  } on the list!`;
+if (inStock.includes(search)) {
+  message = `Yes, we have <strong>${search}</strong>.`;
 } else {
   message = `Sorry, we do not have <strong>${search}</strong>.`;
 }
